3db4dcaa97a5de5b1867f00ff74490aca2320e7b
coskun cakir <csknckr@gmail.com>
2020-04-12 18:30:19 +0100

    dropdown component added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { ANIMALS } from "@frontendmasters/pet";
 
+import useDropDown from "./useDropdown";
+
 const SearchParams = () => {
   const [location, setLocation] = React.useState("Seattle, WA");
-  const [animal, setAnimal] = React.useState("dog");
+  const [breeds, setBreeds] = React.useState([]);
+  const [animal, AnimalDropdown] = useDropDown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropDown("Breed", "", breeds);
+
   return (
     <div className="search-params">
       <form>
@@ -16,22 +21,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
